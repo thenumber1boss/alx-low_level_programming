aabeea54cb42aa5156f70a22786cfb943e861620
thenumber1boss <bamijokoseun@gmail.com>
2022-12-09 15:48:24 -0800

    yet

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-*
 * main - main function in the task
 *
 *
@@ -10,5 +9,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 }
