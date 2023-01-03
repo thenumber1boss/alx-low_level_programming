07807a981e48ef36beef7f4726c370e2562c09ad
thenumber1boss <bamijokoseun@gmail.com>
2023-01-03 14:53:55 -0800

    add

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  *_memset - The memeset() function fills
- *the first n bytes of the memory area
- *pointed to by s with the constant byte b
+ * the first n bytes of the memory area
+ * pointed to by s with the constant byte b
  *@s:target
  *@b:constant byte
  *@n:number of byte
  *Return: returns new value of target
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
 	{
