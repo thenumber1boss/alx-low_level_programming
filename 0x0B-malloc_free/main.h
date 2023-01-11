61bc74c1a02eaf94b1007c36630f9ef48daa90f2
thenumber1boss <bamijokoseun@gmail.com>
2023-01-10 16:37:03 -0800

    no advanced task

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+void free_grid(int **grid, int height);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
+#endif /* MAIN_H */
